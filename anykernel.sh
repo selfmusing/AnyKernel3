72c761338aa3d154a9f639be46352ad7894bb66b
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2023-10-26 16:38:45 +0000

    AK3: Support A13-A14

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ device.name2=
 device.name3=
 device.name4=
 device.name5=
-supported.versions=11.0-13.0
+supported.versions=13.0-14.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
